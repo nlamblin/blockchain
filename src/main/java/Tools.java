0496f8c32e12cbf3d04a9456f27f087615be649a
nlamblin <nicolas.lamblin2@gmail.com>
2019-01-22 13:58:46 +0100

    feature: Class Tools (apply256 and generateHash)

---
+++ src/main/java/Tools.java
@@ -0,0 +1,26 @@
+import java.security.MessageDigest;
+
+class Tools {
+
+    static String applyHash(String data) {
+        try {
+            MessageDigest msgDigest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = msgDigest.digest(data.getBytes("UTF-8"));
+            StringBuilder hexaData = new StringBuilder();
+            for (byte hash1 : hash) {
+                String hexa = Integer.toHexString(0xff & hash1);
+                if (hexa.length() == 1)
+                    hexaData.append('0');
+                hexaData.append(hexa);
+            }
+            return hexaData.toString();
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static String generateHash(String data) {
+        return applyHash(data);
+    }
+}
